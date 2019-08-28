b58f91096cbb788561013d6d295660c1206dc9e1
Ebou Jobe <ebou.jobe@mail.mcgill.ca>
2019-08-27 21:20:05 -0700

    firebase android setup done

--- android/app/src/main/java/com/canndida_diet/MainApplication.java
+++ android/app/src/main/java/com/canndida_diet/MainApplication.java
@@ -3,6 +3,7 @@ package com.canndida_diet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -14,6 +15,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,13 +32,15 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
             new RNGestureHandlerPackage(),
             new SplashScreenReactPackage(),
             new RNLocalizePackage(),
             new KeychainPackage(),
-            new AsyncStoragePackage()
+            new AsyncStoragePackage(),
+            new RNFirebaseAuthPackage()
       );
     }
 
