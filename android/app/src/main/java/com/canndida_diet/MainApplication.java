4e43651da46bb2fe54aa9389f5347ce504b00ae2
Ebou Jobe <dsp@dsps-MacBook-Pro.local>
2019-08-27 19:19:33 -0700

    added a custom tab bar

--- android/app/src/main/java/com/canndida_diet/MainApplication.java
+++ android/app/src/main/java/com/canndida_diet/MainApplication.java
@@ -3,6 +3,7 @@ package com.canndida_diet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new LinearGradientPackage(),
             new RNGestureHandlerPackage(),
             new SplashScreenReactPackage(),
--- app/navigation/tab-bar-navigation.tsx
+++ app/navigation/tab-bar-navigation.tsx
@@ -1,16 +1,104 @@
-import { createBottomTabNavigator } from 'react-navigation';
+import React from "react";
+import Ionicons from 'react-native-vector-icons/FontAwesome';
+
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+} from 'react-navigation';
+
 import Home from '../screens/home'
 import Recipes from '../screens/recipes'
 import Search from '../screens/search'
 import Settings from '../screens/settings'
 import Diary from '../screens/diary'
 
-export default createBottomTabNavigator(
+import TabBar from './tabBar';
+
+
+/* declaration of stacks */
+const HomeStack = createStackNavigator({
+  home: Home
+},
+{
+  headerMode: "none",
+  navigationOptions: { gesturesEnabled: false },
+})
+const RecipesStack = createStackNavigator({
+  recipes: Recipes
+},
+{
+  headerMode: "none",
+  navigationOptions: { gesturesEnabled: false },
+})
+const SearchStack = createStackNavigator({
+  search: Search
+},
+{
+  headerMode: "none",
+  navigationOptions: { gesturesEnabled: false },
+})
+const SettingsStack = createStackNavigator({
+  settings: Settings
+},
   {
-    home: Home,
-    search: Search,
-    diary: Diary,
-    recipes: Recipes,
-    settings: Settings,
-  }
-);
+    headerMode: "none",
+    navigationOptions: { gesturesEnabled: false },
+  })
+const DiaryStack = createStackNavigator({
+  diary: Diary
+},
+{
+  headerMode: "none",
+  navigationOptions: { gesturesEnabled: false },
+})
+
+const Tabs = createBottomTabNavigator(
+  {
+    Home: HomeStack,
+    Search: SearchStack,
+    Diary: DiaryStack,
+    Recipes: RecipesStack,
+    Settings: SettingsStack,
+
+  },{
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `home`;
+        } else if (routeName === 'Recipes') {
+          iconName = `star`;
+        } else if (routeName === 'Search') {
+          iconName = `search`;
+        } else if (routeName === 'Settings') {
+          iconName = `cog`;
+        } else if (routeName === 'Diary') {
+          iconName = `book`;
+        }
+
+        return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+      },
+    }),
+    
+    tabBarComponent: (props) => <TabBar {...props}/>,
+    tabBarOptions: {
+      //@ts-ignore
+        tabFeatured: 'Diary',
+        backgroundFeaturedIcon: '#D7465A',
+        activeFeaturedTintColor: 'skyblue',
+        inactiveFeatureTintColor: 'white',
+        showLabel: true,
+        activeTintColor: '#D7465A',
+        inactiveTintColor: '#E1E3DB',
+        style: {
+            height: 80,
+            backgroundColor: '#FFFFFF',
+            borderTopWidth: 1,
+            borderTopColor: '#F2F3EF'
+        },
+        tabStyle: {}
+    }
+});
+
+export default Tabs
