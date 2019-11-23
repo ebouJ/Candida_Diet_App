0760158fa37ff79eb2b3cc176db5c6dade5676eb
Ebou Jobe <ebou.jobe@mail.mcgill.ca>
2019-11-23 12:24:51 -0500

    installed codepush on ios

--- ios/Canndida_Diet/AppDelegate.m
+++ ios/Canndida_Diet/AppDelegate.m
@@ -14,6 +14,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <Firebase.h>
+#import <CodePush/CodePush.h>
 
 @implementation AppDelegate
 
@@ -43,7 +44,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
